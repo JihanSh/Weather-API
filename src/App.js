c653bcc964ab78172608b6616492f6f3e5cb12ec
Omar Banat <o.j.banat@gmail.com>
2022-12-20 19:10:33 +0200

    Fetch weather from API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 
 import FindWeather from "./components/FindWeather";
@@ -5,11 +7,23 @@ import WeatherDisplay from "./components/WeatherDisplay";
 import DaysWeather from "./components/DaysWeather";
 
 function App() {
+  const [response, setResponse] = useState([]);
+
+  const fetchWeather = (city) => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=8b0085fe50c89792c823f75df807415e`
+      )
+      .then((response) => {
+        setResponse((prevState) => [...prevState, response.data]);
+      });
+  };
+
   return (
     <div className="main">
-      <FindWeather />
-      <WeatherDisplay />
-      <DaysWeather />
+      <FindWeather fetchWeather={fetchWeather} />
+      <WeatherDisplay response={response[0]} />
+      <DaysWeather response={response[0]} />
     </div>
   );
 }
